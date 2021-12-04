4c93491fd5c2c39c372222d5184e4fdd3d099f7c
joaorpereira <jprp17@live.com>
2021-12-04 17:06:01 -0300

    feature: add jest configuration to render svg file

--- jest.config.js
+++ jest.config.js
@@ -8,5 +8,10 @@ module.exports = {
   ],
   setupFilesAfterEnv: ["<rootDir>/.jest/setup.ts"],
   moduleFileExtensions: ["tsx", "ts", "js", "jsx", "json", "node"],
-  moduleDirectories: ["node_modules", "src"]
+  moduleDirectories: ["node_modules", "src"],
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/__mocks__/fileMock.ts",
+    "\\.(css|scss)$": "<rootDir>/__mocks__/styleMock.js"
+  }
 };
