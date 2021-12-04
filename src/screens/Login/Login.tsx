b8ec1f00c51e063c266f37dc90bd17b3f77049d1
joaorpereira <jprp17@live.com>
2021-12-04 17:04:50 -0300

    feature: create layot of Login page

--- src/screens/Login/Login.tsx
+++ src/screens/Login/Login.tsx
@@ -1,7 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import Input from "components/common/Input/Input";
+import Logo from "assets/logo.svg";
+import Button from "components/common/Button/Button";
 
-const Home = () => {
-  return <div>Home</div>;
+import * as S from "./styled";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+  const handleEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const handleLogin = () => {
+    if (email === "" && password === "") {
+      setErrorMessage("Email and password are Required");
+    } else if (password === "") {
+      setErrorMessage("Password is Required");
+    } else if (email === "") {
+      setErrorMessage("Email is Required");
+    }
+  };
+
+  return (
+    <S.Wrapper>
+      <S.LoginContainer>
+        <S.Image src={Logo} alt="Unsplash Logo" />
+        <Input
+          margin="20px 0"
+          value={email}
+          name="email"
+          placeholder="Email"
+          onChange={(e) => handleEmail(e)}
+        />
+        <Input
+          margin="20px 0 50px 0px"
+          value={password}
+          name="password"
+          placeholder="Password"
+          onChange={(e) => handlePassword(e)}
+        />
+        {errorMessage ? <p>{errorMessage}</p> : null}
+        <Button onClick={handleLogin} color="primary" label="Login" />
+      </S.LoginContainer>
+    </S.Wrapper>
+  );
 };
 
-export default Home;
+export default Login;
---
+++ src/screens/Login/styled.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.main`
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const LoginContainer = styled.div`
+  width: 500px;
+  height: 500px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+export const Image = styled.img`
+  height: 150px;
+  margin-bottom: 50px;
+`;
