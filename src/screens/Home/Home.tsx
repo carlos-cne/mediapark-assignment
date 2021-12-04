2d7f20f183dd3b68cd98b0e41085505d83747106
joaorpereira <jprp17@live.com>
2021-12-04 20:59:44 -0300

    feature: finish all login process of the user

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,6 +1,48 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
+import { useLocation } from "react-router";
+import api from "services/api";
 
 const Home = () => {
+  const [loading, setLoading] = useState(false);
+  const locations = useLocation();
+  const [, code] = locations.search.split("=");
+
+  const handleLogin = useCallback(async () => {
+    setLoading(true);
+    try {
+      const res = api.post(
+        `https://unsplash.com/oauth/token?client_id=${process.env.REACT_APP_ACCESS_KEY}&client_secret=${process.env.REACT_APP_SECRET_KEY}&redirect_uri=${process.env.REACT_APP_BASE_URL}&code=${code}&grant_type=authorization_code`
+      );
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  }, [code]);
+
+  useEffect(() => {
+    if (code) {
+      handleLogin();
+    }
+  }, [code, handleLogin]);
+
+  if (loading) {
+    return (
+      <div
+        style={{
+          height: "100vh",
+          width: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center"
+        }}
+      >
+        Loading...
+      </div>
+    );
+  }
+
   return <div>Home</div>;
 };
 
