7c95387d6fc35212d0470dad5b2a071c2853af0c
joaorpereira <jprp17@live.com>
2021-12-04 20:58:13 -0300

    chore: setup env variables to jest test

---
+++ .jest/setEnvVars.ts
@@ -0,0 +1,5 @@
+process.env.REACT_APP_ACCESS_KEY='BcU_G72479QRZ26cZQl89_a5q3SxZe9TCGQVE0vXjLw'
+process.env.REACT_APP_SECRET_KEY='7vRTFMBlTFxOHO1j5qb-vYj2KUvNr2-DVlgFCStjjgw'
+process.env.REACT_APP_REDIRECT_URL='urn:ietf:wg:oauth:2.0:oob'
+process.env.REACT_APP_BASE_URL='http://localhost:3000/'
+process.env.REACT_APP_AUTH_URL='https://unsplash.com/oauth/authorize'
