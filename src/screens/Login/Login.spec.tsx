0dd6977c2b503e7012965719382d370475a43dd5
joaorpereira <jprp17@live.com>
2021-12-04 21:56:02 -0300

    test: move logic of login page from Home page to login Page

--- src/screens/Login/Login.spec.tsx
+++ src/screens/Login/Login.spec.tsx
@@ -2,22 +2,35 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import Login from "./Login";
 import userEvent from "@testing-library/user-event";
+import { Provider } from "context/provider/Provider";
 
 describe("<Login/>", () => {
   it("Should render email and password inputs", () => {
-    render(<Login />);
+    render(
+      <Provider>
+        <Login />
+      </Provider>
+    );
 
     expect(screen.getByPlaceholderText("Email")).toBeInTheDocument();
     expect(screen.getByPlaceholderText("Password")).toBeInTheDocument();
   });
 
   it("Should render Logo image", () => {
-    render(<Login />);
+    render(
+      <Provider>
+        <Login />
+      </Provider>
+    );
     expect(screen.getByAltText("Unsplash Logo")).toBeInTheDocument();
   });
 
   it("should render error message 'Email and password are Required' when try to login with empty values for email and password", () => {
-    render(<Login />);
+    render(
+      <Provider>
+        <Login />
+      </Provider>
+    );
     userEvent.click(screen.getByRole("button", { name: "Log in" }));
 
     const errorMessage = screen.getByText("Email and password are Required");
@@ -25,7 +38,11 @@ describe("<Login/>", () => {
   });
 
   it("should render error message 'Password is Required' when try to login empty value for email", () => {
-    render(<Login />);
+    render(
+      <Provider>
+        <Login />
+      </Provider>
+    );
     const inputEmail = screen.getByPlaceholderText("Email");
     userEvent.type(inputEmail, "joaopaulo@email.com");
     expect(inputEmail).toHaveValue("joaopaulo@email.com");
@@ -37,7 +54,11 @@ describe("<Login/>", () => {
   });
 
   it("should render error message 'Email is Required' when try to login empty value for email", () => {
-    render(<Login />);
+    render(
+      <Provider>
+        <Login />
+      </Provider>
+    );
     const inputPassword = screen.getByPlaceholderText("Password");
     userEvent.type(inputPassword, "123456");
     expect(inputPassword).toHaveValue("123456");
@@ -49,7 +70,11 @@ describe("<Login/>", () => {
   });
 
   // it("should render error message 'Email or Password incorrect' when try to login empty value for email", async () => {
-  //   render(<Login />);
+  //     render(
+  //   <Provider>
+  //     <Login />
+  //   </Provider>
+  // );
   //   const testErrorMessage = 'THIS IS A TEST FAILURE'
 
   //   const inputEmail = screen.getByPlaceholderText("Email");
