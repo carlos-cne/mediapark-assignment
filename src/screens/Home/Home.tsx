0b1361ee0aa32bf2641e4dee829ce36d64f47796
joaorpereira <jprp17@live.com>
2021-12-05 15:57:02 -0300

    feature: add Search component to Home page

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,13 +1,30 @@
 import Navbar from "components/Navbar/Navbar";
+import Search from "components/Search/Search";
 import AuthContext from "context/provider/AuthProvider";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
+
+import * as S from "./styled";
 
 const Home = () => {
   const { handleRemoveToken } = useContext(AuthContext);
 
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleSearchValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.target.value);
+  };
+  const handleSubmitSearch = () => {};
+
   return (
     <main>
       <Navbar onClick={handleRemoveToken} />
+      <S.Div>
+        <Search
+          onChange={(e) => handleSearchValue(e)}
+          onClick={handleSubmitSearch}
+          value={searchValue}
+        />
+      </S.Div>
     </main>
   );
 };
