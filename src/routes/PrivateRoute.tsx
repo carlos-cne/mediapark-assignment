51e067d5d67dc531b00adb71cc2576891d499e90
joaorpereira <jprp17@live.com>
2021-12-04 21:55:06 -0300

    feature: finish PrivateRoute

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -1,4 +1,5 @@
-import React, { FC, ReactElement } from "react";
+import AuthContext from "context/provider/AuthProvider";
+import React, { FC, ReactElement, useContext } from "react";
 import { Navigate } from "react-router-dom";
 
 type Props = {
@@ -6,7 +7,8 @@ type Props = {
 };
 
 const PrivateRoute: FC<Props> = ({ children }) => {
-  const token = null;
+  const { token } = useContext(AuthContext);
+
   return token ? children : <Navigate to={"/login"} />;
 };
 
