e67b99651b1297532048efdb8651c3b206fcfec4
joaorpereira <jprp17@live.com>
2021-12-05 17:48:30 -0300

    test: create UserInfo Component

---
+++ src/components/UserInfo/UserInfo.spec.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import UserInfo from "./UserInfo";
+
+describe("<UserInfo/>", () => {
+  it("should render image and user name correctly", () => {
+    render(
+      <UserInfo
+        name="Oscar Sutton"
+        profile_photo="https://images.unsplash.com/photo-1530281700549-e8…58MHx8fHwxNjM4NzM1NTA0&ixlib=rb-1.2.1&q=80&w=1080"
+      />
+    );
+    expect(screen.getByAltText("Oscar Sutton")).toBeInTheDocument();
+    expect(screen.getByText("Oscar Sutton")).toBeInTheDocument();
+  });
+});
