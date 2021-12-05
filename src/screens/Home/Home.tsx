2613b3e6aabedc9cf2a8cce630cca709b88ace75
joaorpereira <jprp17@live.com>
2021-12-04 21:56:23 -0300

    refactor: remove login logic from Home page

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,49 +1,14 @@
-import React, { useCallback, useEffect, useState } from "react";
-import { useLocation } from "react-router";
-import api from "services/api";
+import AuthContext from "context/provider/AuthProvider";
+import React, { useContext } from "react";
 
 const Home = () => {
-  const [loading, setLoading] = useState(false);
-  const locations = useLocation();
-  const [, code] = locations.search.split("=");
+  const { handleRemoveToken } = useContext(AuthContext);
 
-  const handleLogin = useCallback(async () => {
-    setLoading(true);
-    try {
-      const res = api.post(
-        `https://unsplash.com/oauth/token?client_id=${process.env.REACT_APP_ACCESS_KEY}&client_secret=${process.env.REACT_APP_SECRET_KEY}&redirect_uri=${process.env.REACT_APP_BASE_URL}&code=${code}&grant_type=authorization_code`
-      );
-      console.log(res);
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setLoading(false);
-    }
-  }, [code]);
-
-  useEffect(() => {
-    if (code) {
-      handleLogin();
-    }
-  }, [code, handleLogin]);
-
-  if (loading) {
-    return (
-      <div
-        style={{
-          height: "100vh",
-          width: "100%",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center"
-        }}
-      >
-        Loading...
-      </div>
-    );
-  }
-
-  return <div>Home</div>;
+  return (
+    <div>
+      Home<button onClick={handleRemoveToken}>Logout</button>
+    </div>
+  );
 };
 
 export default Home;
