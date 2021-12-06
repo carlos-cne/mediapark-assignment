88ad1941a3f55a49c2391c98a9ce035d3ca70521
joaorpereira <jprp17@live.com>
2021-12-05 22:27:51 -0300

    test: create Dropdown component

---
+++ src/components/Dropdown/Dropdown.spec.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Dropdown from "./Dropdown";
+
+describe("<DescriptionSection/>", () => {
+  it("should render Dropdown with correct properties", () => {
+    render(<Dropdown suggestions={["dog", "cat"]} setSearchValue={jest.fn} />);
+
+    expect(screen.getByText("dog")).toBeInTheDocument();
+    expect(screen.getByText("cat")).toBeInTheDocument();
+  });
+});
