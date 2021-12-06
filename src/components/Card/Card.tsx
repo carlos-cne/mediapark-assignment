d8f44239c3cd02484ea747e7f4c55001c64c16f0
joaorpereira <jprp17@live.com>
2021-12-05 22:20:46 -0300

    refactor: change Card component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,13 +1,29 @@
-import React, { FC, ReactNode } from "react";
+import React, { FC } from "react";
+import DescriptionSection from "components/DescriptionSection/DescriptionSection";
+import ImageSection from "components/ImageSection/ImageSection";
+import UserInfoSection from "components/UserInfoSection/UserInfoSection";
+import { PhotoProps } from "screens/Home/Home";
+
 import * as S from "./styled";
 
 type Props = {
-  children: ReactNode;
+  photo: PhotoProps;
 };
 
-const Card: FC<Props> = ({ children }) => {
+const Card: FC<Props> = ({ photo }) => {
   return (
-    <S.CardWrapper data-testid="custom-modal-element">{children}</S.CardWrapper>
+    <S.CardWrapper data-testid="custom-modal-element">
+      <UserInfoSection
+        profile_photo={photo.user.profile_photo}
+        name={photo.user.name}
+      />
+      <ImageSection
+        photo={photo.photo}
+        alt_description={photo.alt_description}
+        likes={photo.likes}
+      />
+      <DescriptionSection description={photo.description} />
+    </S.CardWrapper>
   );
 };
 
